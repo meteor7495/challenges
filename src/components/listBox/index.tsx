099553a58995b76771e41fb68c4fd107935b2a99
p.sheikhrahimi <pedramshrahimi@gmail.com>
2025-01-21 15:56:50 +0330

    feat: add ability to save items in local storage

--- src/components/listBox/index.tsx
+++ src/components/listBox/index.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Form from "../form";
 import List from "../list";
 import { useStyles } from "./style";
@@ -13,12 +13,35 @@ function ListBox() {
       isChecked: boolean;
       title: string;
     }[]
-  >([]);
+  >();
+
+  useEffect(() => {
+    async function asyncCall() {
+      const localDataList: {
+        id: string;
+        isChecked: boolean;
+        title: string;
+      }[] = await JSON.parse(localStorage.getItem("localDataList") || "[]");
+
+      setDataList(localDataList);
+    }
+
+    asyncCall();
+  }, []);
+
+  const stringDataList = JSON.stringify(dataList);
+
+  useEffect(() => {
+    if (dataList !== undefined) {
+      localStorage.setItem("localDataList", JSON.stringify(dataList));
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [stringDataList]);
 
   return (
     <div className={classes.listBoxContainer}>
       <Form setDataList={setDataList} />
-      <List dataList={dataList} />
+      <List dataList={dataList} setDataList={setDataList} />
     </div>
   );
 }
