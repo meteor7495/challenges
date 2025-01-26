3a43c11974c05a5bdf03dff853a5fd88a7093b15
m.hassanzadeh <m.hassanzadeh@netbar.ir>
2025-01-26 21:11:55 +0330

    init

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import {Stack, Text} from "@mantine/core";
+
+export default async function Page() {
+
+    return (
+      <Stack >
+          <Text>مجیبسییسبی</Text>
+      </Stack>
+    );
+}
